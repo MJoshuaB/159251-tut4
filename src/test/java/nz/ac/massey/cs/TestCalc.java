95740d87b2b72d776ef3318645496344cb58daf6
MJoshuaB <13187637+MJoshuaB@users.noreply.github.com>
2021-08-10 10:48:50 +1200

    add test for Calc.subtract

--- src/test/java/nz/ac/massey/cs/TestCalc.java
+++ src/test/java/nz/ac/massey/cs/TestCalc.java
@@ -9,6 +9,12 @@ class TestCalc {
     @Test
     void TestAdd() {
         Calc calc = new Calc();
-        assertEquals(2, calc.add(1, 1));
+        assertEquals(2, calc.add(1, 1), "1 + 1 shoud be 2");
+    }
+
+    @Test
+    void TestSubtract() {
+        Calc calc = new Calc();
+        assertEquals(0, calc.subtract(1, 1), "1 - 1 should be 0");
     }
 }
